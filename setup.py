353e6cf5ebc1fc6b7f0c9dad2f8d60dd3321ee4c
Yebulabula <47183066+Yebulabula@users.noreply.github.com>
2023-03-15 14:27:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="guided-diffusion",
     py_modules=["guided_diffusion"],
-    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    install_requires=["torch", "tqdm", "matplotlib","numpy","torchvision","scikit-image", "scipy", "blobfile", "cv2"],
 )
