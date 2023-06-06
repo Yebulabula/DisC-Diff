968c2ef2ba8174f300155023625288c09c3ca6d8
Y Mao <47183066+Yebulabula@users.noreply.github.com>
2023-06-06 17:57:35 +0100

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="guided-diffusion",
-    py_modules=["guided_diffusion"],
-    install_requires=["torch", "tqdm", "matplotlib","numpy","torchvision","scikit-image", "scipy", "blobfile", "cv2"],
-)
